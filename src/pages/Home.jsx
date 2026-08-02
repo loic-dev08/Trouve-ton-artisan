6213be15194875a8f2192bb3f2eb61657318fb85
Loic <leotetloic08@gmail.com>
2026-08-02 21:57:15 +0200

    ajout de la page Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+import { useState, useEffect } from "react";
+import {useNavigate} from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import ProCard from "../components/ProCard";
+import StarRating from "../components/StarRating";
+import api from "../api/api";
+import  styles from "../styles/Home.module.css";
