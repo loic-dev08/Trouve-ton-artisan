5055da1495e6c85983096c78ad9844e275de2a8d
Loic <leotetloic08@gmail.com>
2026-09-03 11:09:28 +0200

    Ajout de ArtisanCard.jsx

---
+++ src/components/ArtisanCard.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import Etoiles from"../components/Etoiles";
+import { getCategorieOfArtisan, getSpecialite } from "../../data/mockData";
+import "./ArtisanCard.scss";
+
+export default function ArtisanCard({ artisan }) {
+  const categorie = getCategorieOfArtisan(artisan);
+  const specialite = getSpecialite(artisan.specialiteId);
+
+    return (
+        <Link
+            to={`/artisan/${artisan.id}`}
+            className={`artisan-card category-${categorie?.slug}`}
+        >
+            <div className="artisan-card__image">
+                <img src={artisan.image} alt="" loading="lazy" />
+            </div>
+            <div className="artisan-card__body">
+                <p className="artisan-card__categorie">{categorie?.nom}</p>
+                <h3 className="artisan-card__nom">{artisan.nom}</h3>
+                <p className="artisan-card__specialite">{specialite?.nom}</p>
+                <Etoiles note={artisan.note} />
+                <p className="artisan-card__ville">{artisan.ville}</p>
+            </div>
+        </Link>
+    );
+}
